e8c3a667ef1044826d3a3251d09c0a27fe9f8f2a
isadora <161738122+iF08163742@users.noreply.github.com>
2024-10-31 11:46:27 -0300

    PROJETO FINAL

---
+++ script.js
@@ -0,0 +1,77 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas =
+    {
+        enunciado: "Morreu um familiar seu e você foi ao sepultamento, ao final do enterro você se encaminha em direção ao portão de saída e se depara com uma noiva com vestido branco todo sujo de sangue. O que você faz??"[
+            alternativas, [
+            {
+                texto: "Eu fecho os olhos e passo correndo.",
+                afirmacao: "afirmação"
+            }
+        ],
+            
+                [
+                texto, "Eu caminho um pouco mais rápido e passo sem olhar para os lados junto fazendo uma reza na mente.",
+                afirmacao, "afirmação"
+                ]
+            
+        ],
+    }
+    {
+        enunciado: "Ufa, você conseguiu entrar em seu carro, porém ao dar partida você nota que ele não está ligando e aquela noiva que estava na saída do cemitério está vindo em direção ao su carro. O que você faz??",
+        alternativas, [
+    {
+                texto: "Eu entro em crise e saio correndo sem direção e abandono o carro."
+                afirmacao: "afirmação",
+               
+    }
+    {
+                texto: "Eu tento o mais rápido possível ligar para alguém que possa me socorrer."
+                afirmacao: "afirmação"
+            }
+        ]
+    }
+;
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+if(atual >= perguntas.length){
+mostraResultado();
+return;
+    }
+    perguntaAtual = perguntas[atual]
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+for(const alternativa of perguntaAtual.alternativas){
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa.texto;
+    botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+caixaAlternativas.appendChild(botaoAlternativas);
+}
+}
+
+function respostaSelecionada(opçaoSelecionada){
+    const afirmacoes = opçaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado(){
+caixaPerguntas.textContent = "em 2050...";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
